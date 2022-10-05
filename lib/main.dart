e8c8e5da1e0f2368e7fcd2632bbfcc6cf5fa758e
luwanhiika <luwanhika@hotmail.com>
2022-10-05 00:19:21 -0300

    Validando o token e persistindo os dados

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,22 @@ import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages_routes/app_pages.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+
   Get.put(AuthController());
 
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.testMode = true;
     return GetMaterialApp(
       title: 'Greengrocer',
       theme: ThemeData(
--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -15,6 +15,13 @@ class AuthController extends GetxController {
 
   UserModel user = UserModel();
 
+  @override
+  void onInit() {
+    super.onInit();
+
+    validateToken();
+  }
+
   Future<void> validateToken() async {
     // Recuperar o token que foi salvo localmente
     String? token = await utilsServices.getLocalData(key: StorageKeys.token);
