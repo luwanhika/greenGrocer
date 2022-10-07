4beb2f3e05cd727f7644d2c46fa30c6528d9654d
luwanhiika <luwanhika@hotmail.com>
2022-10-06 23:01:00 -0300

    Criando os validators

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Get.testMode = true;
+    // Get.testMode = true;
     return GetMaterialApp(
       title: 'Greengrocer',
       theme: ThemeData(
--- lib/src/pages/auth/view/sign_in_creen.dart
+++ lib/src/pages/auth/view/sign_in_creen.dart
@@ -6,6 +6,7 @@ import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages/cpmmon_widgets/app_name_widget.dart';
 import 'package:greengrocer/src/pages_routes/app_pages.dart';
 
+import '../../../services/validators.dart';
 import '../../cpmmon_widgets/custom_text_field.dart';
 
 class SignInScreen extends StatelessWidget {
@@ -86,15 +87,7 @@ class SignInScreen extends StatelessWidget {
                         controller: emailController,
                         icon: Icons.email,
                         label: 'Email',
-                        validator: (email) {
-                          if (email == null || email.isEmpty) {
-                            return 'Digite seu email!';
-                          }
-
-                          if (!email.isEmail) return 'Digite um email válido!';
-
-                          return null;
-                        },
+                        validator: emailValidator,
                       ),
 
                       // Senha
@@ -103,16 +96,7 @@ class SignInScreen extends StatelessWidget {
                         icon: Icons.lock,
                         label: 'Senha',
                         isSecret: true,
-                        validator: (password) {
-                          if (password == null || password.isEmpty) {
-                            return 'Digite sua senha!';
-                          }
-
-                          if (password.length < 7) {
-                            return 'Digite uma senha com pelo menos 7 caracteres.';
-                          }
-                          return null;
-                        },
+                        validator: passwordValidator,
                       ),
 
                       // Botão de entrar
