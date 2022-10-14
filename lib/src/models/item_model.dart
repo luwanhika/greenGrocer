0e38dd87557883fe77ef6902b0ed56b815be275d
luwanhiika <luwanhika@hotmail.com>
2022-10-14 15:58:26 -0300

    Recuperando produtos

--- lib/src/models/item_model.dart
+++ lib/src/models/item_model.dart
@@ -1,11 +1,10 @@
-
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:json_annotation/json_annotation.dart';
 
 part 'item_model.g.dart';
 
 @JsonSerializable()
 class ItemModel {
-
   String id;
 
   @JsonKey(name: 'title')
@@ -13,7 +12,7 @@ class ItemModel {
 
   @JsonKey(name: 'picture')
   String imgUrl;
-  
+
   String unit;
   double price;
   String description;
@@ -27,8 +26,12 @@ class ItemModel {
     required this.unit,
   });
 
-  factory ItemModel.fromJson(Map<String, dynamic> json) => _$ItemModelFromJson(json);
+  factory ItemModel.fromJson(Map<String, dynamic> json) =>
+      _$ItemModelFromJson(json);
   Map<String, dynamic> toJson() => _$ItemModelToJson(this);
 
-
+  @override
+  String toString() {
+    return 'ItemModel(id: $id, itemName: $itemName, imgUrl: $imgUrl, unit: $unit, price: $price, description: $description)';
+  }
 }
--- lib/src/pages/home/controller/home_controller.dart
+++ lib/src/pages/home/controller/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:greengrocer/src/models/category_model.dart';
+import 'package:greengrocer/src/models/item_model.dart';
 import 'package:greengrocer/src/pages/home/repository/home_repository.dart';
 import 'package:greengrocer/src/pages/home/result/home_result.dart';
 import 'package:greengrocer/src/services/utils_services.dart';
@@ -28,6 +29,8 @@ class HomeController extends GetxController {
   void selectCategory(CategoryModel category) {
     currentCategory = category;
     update();
+
+    getAllProducts();
   }
 
   Future<void> getAllCategories() async {
@@ -45,6 +48,33 @@ class HomeController extends GetxController {
         if (allCategories.isEmpty) return;
 
         selectCategory(allCategories.first);
+      },
+      error: (message) {
+        utilsServices.showToast(
+          message: message,
+          isError: true,
+        );
+      },
+    );
+  }
+
+  Future<void> getAllProducts() async {
+    setLoading(true);
+
+    Map<String, dynamic> body = {
+      "page": 0,
+      "title": null,
+      "categoryId": "5mjkt5ERRo",
+      "itemsPerPage": 6
+    };
+
+    HomeResult<ItemModel> result = await homeRepository.getAllProducts(body);
+
+    setLoading(false);
+
+    result.when(
+      success: (data) {
+        print(data);
       },
       error: (message) {
         utilsServices.showToast(
